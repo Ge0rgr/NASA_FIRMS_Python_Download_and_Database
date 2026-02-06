2de9230155319114f8c7843bf36baa7bdd01bf90
Ge0rgr <Ge0rgruf@web.de>
2026-02-06 18:30:21 +0100

    follow the first folder path; Choose Data-Collection

--- DataDownloader.py
+++ DataDownloader.py
@@ -3,8 +3,9 @@ import sqlite3
 import re
 import sys
 import time
+from bs4 import BeautifulSoup
 
-
+'''
 ######    Go to nasa login site and log in
 
 go('https://urs.earthdata.nasa.gov/')
@@ -65,7 +66,7 @@ while True:
         continue
 
 #Delay for visual purposes
-time.sleep(2)
+#time.sleep(2)
 
 ####          Go to nasa user-profile to retrieve the username
 
@@ -96,28 +97,88 @@ for char in username:
 
 print("\n")
 
-time.sleep(2)
+#time.sleep(2)
+'''
+
+####     Choose the Data-Collection that you want to get the Data from
+
+go("https://nrt3.modaps.eosdis.nasa.gov/archive/FIRMS")
+
+data_content = browser.html  # Capture raw HTML
+
+soup = BeautifulSoup(data_content,"html.parser")
+data_links = []
+
+#Retrieve specific links that are saved in html and add them to a list
+for link in soup.find_all("tr"):
+    if link.has_attr("data-href"):
+        data_links.append(link["data-href"])
+
+
+# remove unwanted links from the list
+substring = "https" 
+
+for link in data_links:
+    if not substring in link:
+        data_links.remove(link)
+
+
+print(data_links)
+
+#Save just the Folder names for visual presentation (and to get used to regex)
+data = []
+
+for foldername in data_links:
+    x = re.findall(r"/([^/]+)/$",foldername) #has a slash, then extract that (what has one or more non slash characters); and has a Slash at the end, where after that, nothing follows
+    data.append(x)
+
+print("\n\n From which Data-Collection do you want to download the Data?\n")
+
+for i,val in enumerate(data):
+    print(i,val)
+
+
+folder = int(input("\n Please enter the number on the left of the desired folder: "))
+
+while True:
+    if folder not in range(0,len(data_links)):
+        print("\n Please enter a valid number (0 -",len(data_links)-1,") : ")
+        folder = int(input())
+    elif folder in range(0,len(data_links)):
+        break
 
 
 
+###   Enter the folder that has been chosen
+print("Entering folder: ", data[folder], "\n")
 
+three_dots = "..."
+for char in three_dots:
+    print(char,end="",flush=True)
+    time.sleep(0.3)
+
+print("\n")
+
+follow(data_links[folder])
+ 
+
+
+
+###   Choose the Region you want the Data for
+
+
+'''
 #Logout from Website
 
 go("https://urs.earthdata.nasa.gov/logout")
 print("\n Successfully logged out from the Website\n")
 
 
-'''
-
-
-go("https://urs.earthdata.nasa.gov/users/")
 
 
 
-html_content = browser.html  # Capture raw HTML
-html_words = html_content.split() #Split words of html into a list
 
-
+#####    for download token
 #baseurl = "https://urs.earthdata.nasa.gov/users/"
 
 #html = urllib.request.urlopen("https://urs.earthdata.nasa.gov/users/")
