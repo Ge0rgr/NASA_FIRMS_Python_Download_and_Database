cd9238155cb9d5ffb6fc3ede14baf39535954ff9
Ge0rgr <Ge0rgruf@web.de>
2026-02-06 15:16:38 +0100

    retrieve Username and added logout after each session

--- DataDownloader.py
+++ DataDownloader.py
@@ -1,8 +1,13 @@
 from twill.commands import *
 import sqlite3
 import re
+import sys
+import time
 
-go('https://urs.earthdata.nasa.gov/oauth/authorize?response_type=code&client_id=sXTOeM-6RH5fuK3TacT_Ag&redirect_uri=https%3A%2F%2Fnrt3.modaps.eosdis.nasa.gov%2Foauth%2Fcallback&state=E53GLmbJWzjzsbXk2FG7XK9MeSjk1v')
+
+######    Go to nasa login site and log in
+
+go('https://urs.earthdata.nasa.gov/')
 
 mail = ""
 passw = ""
@@ -11,12 +16,11 @@ passw = ""
 #User has to make an input for mail and password; if you cannot log in, you have to re-enter the mail and password
 while True:
 
-
     if mail == "" :
         mail = input('Please enter your email (Type "." if you do not wish to continue): ')
 
         if mail == ".":
-            break
+            sys.exit()
 
         y = bool(re.fullmatch(r"^[^@\s]+@{1}[^@\s]+$",mail)) #Check if it's a valid mail (simple check, no more than one @ and no whitespace)
         if y == False:
@@ -27,43 +31,83 @@ while True:
         passw = input("Please enter your password: ")
         continue
 
+    #Enters the username and password in the form
     fv("1", "username", mail)
     fv("1", "password", passw)
 
 
-    submit()
+    submit() #presses the commit button to log in
 
 
     login_content = browser.html  # Capture raw HTML
-    login_lines = login_content.split("\n") #Split words of html into a list
+    login_lines = login_content.split("\n") #Split lines of html into a list
 
 
-    substring = "Invalid username or password" #Will be searched for in the html document in order to retrieve the Username
+    substring = "Invalid username or password" #Will be searched for in the html document in order to retrieve the info if login was successful or not
 
-    #go through enumerated list and find the index of the word in sp, which contains the Substring Username. If not found: enter -1
-    tmp = next((i for i, word in enumerate(login_lines) if substring in word),-1)
 
-    if tmp == -1:
-        print("Success, you're logged in")
+    failed = False
+
+    #if substring is found, then login has failed
+    for lines in login_lines:
+        if substring in lines:
+            failed = True
+
+
+    if failed == False:
+        print("\nSuccess, you're logged in!\n")
         break
-    elif tmp != -1:
-        print("Wrong e-mail or password. Try Again. ")
+
+    elif failed == True:
+        print("\n Wrong e-mail or password. Try Again. \n")
         mail = ""
         passw = ""
         continue
-    
+
+#Delay for visual purposes
+time.sleep(2)
+
+####          Go to nasa user-profile to retrieve the username
+
+go('https://urs.earthdata.nasa.gov/profile')
+
+html_content = browser.html  # Capture raw HTML
+html_words = html_content.split() #Split words of html into a list
+
+substring = "Username:" #Will be searched for in the html document in order to retrieve the Username
+
+#go through enumerated list and find the index of the word, which contains the Substring Username. If not found: enter -1
+tmp = next((i for i, word in enumerate(html_words) if substring in word),-1)
+
+#If no Username detected, close the script;  
+if tmp == -1:
+    print("ERROR: No Username found\n")
+    sys.exit("Existing Account without a Username is not possible") 
+
+
+#username is needed to access to download token
+username = html_words[tmp + 1].strip()
+print("\nRetrieving your Username: ")
+
+#Cool way of displaying username
+for char in username:
+    print(char,end="",flush=True)
+    time.sleep(0.3)
+
+print("\n")
+
+time.sleep(2)
+
+
+
+
+#Logout from Website
+
+go("https://urs.earthdata.nasa.gov/logout")
+print("\n Successfully logged out from the Website\n")
+
 
 '''
-    #If no Username detected, close the script
-    if tmp == -1:
-        print("Success")
-        break
-
-    else:
-        print("Login failed. Please enter a valid email and password.")
-        continue
-
-
 
 
 go("https://urs.earthdata.nasa.gov/users/")
@@ -72,20 +116,6 @@ go("https://urs.earthdata.nasa.gov/users/")
 
 html_content = browser.html  # Capture raw HTML
 html_words = html_content.split() #Split words of html into a list
-
-substring = "Username:" #Will be searched for in the html document in order to retrieve the Username
-
-#go through enumerated list and find the index of the word in sp, which contains the Substring Username. If not found: enter -1
-tmp = next((i for i, word in enumerate(html_words) if substring in word),-1)
-
-#If no Username detected, close the script
-if tmp == -1:
-    print("No Username found")
-    exit() 
-
-
-username = html_words[tmp + 1].strip()
-print("Username:", username)
 
 
 #baseurl = "https://urs.earthdata.nasa.gov/users/"
@@ -104,4 +134,5 @@ print("Username:", username)
 #text = document.read().decode()
 #if document.getcode() != 200 :
 #    print("Error code=",document.getcode(), url)
+
 '''
